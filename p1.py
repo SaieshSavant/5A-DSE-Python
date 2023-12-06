365aa8acb7da863d3b5d063e8f599f17fb1f87be
Saiesh Savant <109667903+SaieshSavant@users.noreply.github.com>
2023-12-06 04:28:28 +0000

    started

---
+++ p1.py
@@ -0,0 +1,6 @@
+name=input("Enter your name")
+yob=int(input("Enter your of birth"))
+age=2023-yob
+print("Details")
+print("NAME:",name)
+print("Age:",age)
---
+++ p3.py
@@ -0,0 +1,8 @@
+n=4
+x=1
+for i in range(0,n):
+    for j in range(i+1):
+        print(x,end=" ")
+        x+=1
+    print("\n")
+       
