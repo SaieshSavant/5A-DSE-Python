4071ef3b560e45d6a9c9e01c4632a30add60a84f
Saiesh Savant <109667903+SaieshSavant@users.noreply.github.com>
2023-12-13 04:13:14 +0000

    ff

--- P1.py
+++ P1.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x=np.array(['may','june','july','aug','sep','oct','nov','dec'])
-y=np.array(['23','32','27','50','22','18','11','65'])
-y1=np.array(['12','10','9','15','5','2','3','12'])
+x=['may','june','july','aug','sep','oct','nov','dec']
+y=[23,32,27,50,22,18,11,65]
+x1=['may','june','july','aug','sep','oct','nov','dec']
+y1=[12,10,9,15,5,2,3,12]
+plt.xlabel('month')
+plt.ylabel('visitors')
 
 plt.plot(x,y, label = "line 1")
-plt.plot(x,y1, label = "line 2")
+plt.plot(x1,y1, label = "line 2")
 plt.legend()
 plt.show()
