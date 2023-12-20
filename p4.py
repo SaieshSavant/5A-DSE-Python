7b729a707d6dfd202c9d9c32092e5ab3961a77f5
Saiesh Savant <109667903+SaieshSavant@users.noreply.github.com>
2023-12-20 10:02:33 +0530

    Update p4.py

--- p4.py
+++ p4.py
@@ -16,9 +16,24 @@ print("Minimum value of array a is:",np.min(a))
 print("Maximum value of array b is:",np.max(b))
 print("Mean value of array a is:",np.mean(a))
 print("Standard Deviation of array b is:",np.std(b))
+print("Sum of all the elements in array a: ",np.sum(a))
+
+#reshaping arrays
+c=np.array([[1,2],[3,4],[5,6]])
+print("Array c:")
+print(c)
+print("Reshaped array c(2 rows, 3 columns):")
+print(np.reshape(c,(2,3)))
+
+#transposing arrays
+d=np.array([[1,2,3],[4,5,6]])
+print("Array d:")
+print(d)
+print("Transposed array d:")
+print(np.transpose(d))
+
 
 '''
-Output
 Array a: [1 2 3 4 5]
 Array b: [ 6  7  8  9 10]
 Sum of arrays a and b is: [ 7  9 11 13 15]
@@ -31,4 +46,19 @@ Minimum value of array a is: 1
 Maximum value of array b is: 10
 Mean value of array a is: 3.0
 Standard Deviation of array b is: 1.4142135623730951
+Sum of all the elements in array a:  15
+Array c:
+[[1 2]
+ [3 4]
+ [5 6]]
+Reshaped array c(2 rows, 3 columns):
+[[1 2 3]
+ [4 5 6]]
+Array d:
+[[1 2 3]
+ [4 5 6]]
+Transposed array d:
+[[1 4]
+ [2 5]
+ [3 6]]
 '''
